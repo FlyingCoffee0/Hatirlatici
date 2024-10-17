c2b96e87c1683b3032e9e4ba91e58d0706e40a6e
FlyingCoffee0 <tansu_2uysal@hotmail.com>
2024-10-17 19:00:47 +0300

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:case_codeway/screens/auth/sign_up_screen.dart';
 import 'package:case_codeway/screens/auth/todo/create_todo_screen.dart';
 import 'package:case_codeway/screens/auth/todo/todo_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'screens/auth/sign_in_screen.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
